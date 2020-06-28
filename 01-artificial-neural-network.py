19deff0cf2095adb052687b7de89b84e10ea7205
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-06-28 18:56:44 -0300

    Finished up to train_test_split

--- 01-artificial-neural-network.py
+++ 01-artificial-neural-network.py
@@ -1,1 +1,26 @@
+#Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
+#Import the data set and store it in a pandas DataFrame
+raw_data = pd.read_csv('bank_data.csv')
+x_data = raw_data.iloc[:, 3:-1].values
+y_data = raw_data.iloc[:, -1].values
+
+#Handle categorical data (gender first and then geography)
+    #The Gender column uses label encoding
+from sklearn.preprocessing import LabelEncoder
+label_encoder = LabelEncoder()
+x_data[:, 2] = label_encoder.fit_transform(x_data[:, 2])
+
+    #The Geography column uses One Hot Encoding
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+transformer = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [1])], remainder = 'passthrough')
+x_data = np.array(transformer.fit_transform(x_data))
+
+#Split the data set into training data and test data
+from sklearn.model_selection import train_test_split
+x_training_data, x_test_data, y_training_data, y_test_data = train_test_split(x_data, y_data, test_size = 0.3)
