0a3bb8f1a6e50f03685657bf935d688248a32716
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-07-03 12:09:22 -0300

    added imports and preprocessing

--- 02-convolutional-neural-network.py
+++ 02-convolutional-neural-network.py
@@ -1,1 +1,15 @@
+#Import the necessary libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#Preprocessing the training set
+training_generator = ImageDataGenerator(
+                        rescale = 1/255,
+                        shear_range = 0.2,
+                        zoom_range = 0.2,
+                        horizontal_flip = True)
+
+#Preprocessing the test set
 
