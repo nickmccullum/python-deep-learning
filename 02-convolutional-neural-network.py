e0cda80ac672225409fd8627b69a28ba125d39f7
Nick McCullum <nicholasmccullum@gmail.com>
2020-07-04 14:58:58 -0300

    added all steps from building the CNN

--- 02-convolutional-neural-network.py
+++ 02-convolutional-neural-network.py
@@ -11,5 +11,36 @@ training_generator = ImageDataGenerator(
                         zoom_range = 0.2,
                         horizontal_flip = True)
 
-#Preprocessing the test set
+training_set = training_generator.flow_from_directory('training_data',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
 
+#Preprocessing the test set
+test_generator = ImageDataGenerator(rescale = 1./255)
+test_set = test_generator.flow_from_directory('test_data',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+#Building the artificial neural network
+cnn = tf.keras.models.Sequential()
+
+#Adding the convolutional layer
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+
+#Adding our max pooling layer
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+#Adding another convolutional layer and max pooling layer
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+#Adding Our flattening Layer
+cnn.add(tf.keras.layers.Flatten())
+
+#Adding our full connection layer
+cnn.add(tf.keras.layers.Dense(units=128, activation='sigmoid'))
+
+#Adding our output layer
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
